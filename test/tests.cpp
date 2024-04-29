f580a3d6ef12a8c11487c25956e5d9e39b1643e7
Прусаков Михаил <Mikhas3009>
2024-04-30 00:35:34 +0300

    v 2.3

--- test/tests.cpp
+++ test/tests.cpp
@@ -4,34 +4,34 @@
 
 TEST(MarkovChainTest, TrainFromTextFile) {
     markov::MarkovChain generator;
-    generator.train("test-text.txt", 2);
+    generator.train("test/test-text.txt", 2);
     EXPECT_TRUE(generator.table.empty());
 }
 
 TEST(MarkovChainTest, GeneratedPrefixExistsInTable) {
     markov::MarkovChain generator;
-    generator.train("test-text.txt", 2);
+    generator.train("test/test-text.txt", 2);
     markov::MarkovChain::Prefix prefix = {"Jaga", "Boyaga,"};
     EXPECT_FALSE(generator.table.find(prefix) != generator.table.end());
 }
 
 TEST(MarkovChainTest, GeneratedPrefixHasSuffixes) {
     markov::MarkovChain generator;
-    generator.train("test-text.txt", 2);
+    generator.train("test/test-text.txt", 2);
     markov::MarkovChain::Prefix prefix = {"Jaga", "Boyaga,"};
     EXPECT_TRUE(generator.table[prefix].empty());
 }
 
 TEST(MarkovChainTest, GeneratedTextLength) {
     markov::MarkovChain generator;
-    generator.train("test-text.txt", 2);
+    generator.train("test/test-text.txt", 2);
     std::string result = generator.generate(10, true); 
     EXPECT_EQ(result.size(), 44);
 }
 
 TEST(MarkovChainTest, GenerateTextWithGivenPrefix) {
     markov::MarkovChain generator;
-    generator.train("test-text.txt", 2);
+    generator.train("test/test-text.txt", 2);
     markov::MarkovChain::Prefix prefix = {"Jaga", "Boyaga,"};
     std::string result = generator.generate(5, false); 
     EXPECT_TRUE(result.find("Jaga Boyaga,") != std::string::npos);
@@ -39,7 +39,7 @@ TEST(MarkovChainTest, GenerateTextWithGivenPrefix) {
 
 TEST(MarkovChainTest, GenerateTextWithoutRandomFirst) {
     markov::MarkovChain generator;
-    generator.train("test-text.txt", 2);
+    generator.train("test/test-text.txt", 2);
     std::string result1 = generator.generate(5, false);
     std::string result2 = generator.generate(5, false);
     EXPECT_EQ(result1, result2);
