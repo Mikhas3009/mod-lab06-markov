e05a54f07f3c4020d55c5067df755acb8006cf43
Прусаков Михаил <Mikhas3009>
2024-04-30 00:03:50 +0300

    v 1.2

--- test/AllTests.cpp
+++ test/AllTests.cpp
@@ -1,6 +1,8 @@
 // Copyright 2024 Mikhas3009
 
 #include <gtest/gtest.h>
+#include "../include/textgen.h"
+#include"../src/textgen.cpp"
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
